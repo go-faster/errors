b53f5debb25e69505dcb4dd8c21b6de96fc76818
tdakkota <tanc13@yandex.ru>
2022-05-30 15:55:09 +0300

    feat: add Into and Must generic helpers

---
+++ example_Must_test.go
@@ -0,0 +1,16 @@
+package errors_test
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/go-faster/errors"
+)
+
+func ExampleMust() {
+	r := errors.Must(url.Parse(`https://google.com`))
+	fmt.Println(r.String())
+
+	// Output:
+	// https://google.com
+}
